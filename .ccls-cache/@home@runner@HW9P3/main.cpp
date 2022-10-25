010f9c418ceebbfb85f6fb2ec8c65cbe4c1a5abc
AshlyndH <112569560+AshRinn@users.noreply.github.com>
2022-10-25 22:40:35 +0000

    fin main

--- .ccls-cache/@home@runner@HW9P3/main.cpp
+++ .ccls-cache/@home@runner@HW9P3/main.cpp
@@ -2,16 +2,38 @@
 using namespace std;
 
 ////////////funtion prototype////////////
-int tot_points(const char answ[][5], num_tests);
+int tot_points(const char answ[][5], int num_tests);
 
 
 int main()
 {
 
+char tests[6][5]=
+{
+  {'T', 'F', 'T', 'T', 'T'},
+  {'T', 'T', 'T', 'T', 'T'},
+  {'T', 'T', 'F', 'F', 'T'},
+  {'F', 'T', 'F', 'F', 'F'},
+  {'F', 'F', 'F', 'F', 'F'},
+  {'T', 'T', 'F', 'T', 'F'}
+};
+  int points = tot_points(tests, 6);
+  for (int j = 0; j++);
+    cout<< points[j]<< " ,";
+
+  return 0;
   
 }
 
-int tot_points(const char answ[][5], num_tests)
+int tot_points(const char answ[][5], int num_tests)
 {
   const char answ[5] = {'T', 'T', 'F', 'F', 'T'};
+  int points = new int[num_tests]();
+  const int point = 5;
+
+  for (j = 0; j < num_tests; j++)
+  for (i = 0; i < 5; i++)
+    if(answ[j][i] == answ[i])
+      points[j] += point;
+  return points;
 }
